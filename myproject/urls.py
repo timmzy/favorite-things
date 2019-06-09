1cf10aafc4f37730c4c5fcf0863c159e98c993e1
Timmzy <dapwill@gmail.com>
2019-06-09 04:37:18 +0100

    Fix Staticfiles

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from favorite_app import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('favorite_app.urls', namespace='api')),
     path('', views.home, name='home')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
