24a17558fbb161a7ddfc13b0e874b68a5b135bcd
Timmzy <dapwill@gmail.com>
2019-06-09 06:20:47 +0100

    Fix

--- myproject/prod.py
+++ myproject/prod.py
@@ -15,7 +15,7 @@ DATABASES = {
     }
 }
 
-ALLOWED_HOSTS = ["52.15.224.156", "ec2-52-15-224-156.us-east-2.compute.amazonaws.com"]
+ALLOWED_HOSTS = ["52.15.224.156", "ec2-52-15-224-156.us-east-2.compute.amazonaws.com", "*"]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = "/static/"
