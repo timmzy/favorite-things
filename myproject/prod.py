291a5585fd9c41b1f3d6d0a2fad67988745877c5
Timmzy <dapwill@gmail.com>
2019-06-09 12:11:04 +0100

    Static root

--- myproject/prod.py
+++ myproject/prod.py
@@ -17,5 +17,5 @@ DATABASES = {
 
 ALLOWED_HOSTS = ["52.15.224.156", "ec2-52-15-224-156.us-east-2.compute.amazonaws.com", "*"]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = "/static/"
